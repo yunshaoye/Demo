eb4f273db909277186a4953ec2a2685f1f3bea99
昙花未现 <mo@yue.ning>
2021-09-12 18:22:57 +0800

    second

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("git1");
+        System.out.println("git2");
     }
 }
